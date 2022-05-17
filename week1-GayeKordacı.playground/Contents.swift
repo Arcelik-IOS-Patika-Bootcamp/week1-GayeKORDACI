288dce2f410d2ee946b486f36f5848ebdb0ebe33
GayeKORDACI <13641787+GayeKORDACI@users.noreply.github.com>
2022-05-18 00:18:30 +0300

    Packages enum
    added package names

--- week1-GayeKordacı.playground/Contents.swift
+++ week1-GayeKordacı.playground/Contents.swift
@@ -1,7 +1,13 @@
 import UIKit
 
-var str = "Hello, playground"
+var str = "1. Hafta Ödevi"
 
-
+enum PackageName :Int{
+    case prime = 0
+    case primeSelection = 1
+    case allureSelection = 2
+    case gt = 3
+    
+}
 
 
